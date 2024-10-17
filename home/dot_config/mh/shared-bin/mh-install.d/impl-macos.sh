c7f646701d1af730a72dbfcbcc3707d6e092c1ff
Matt Hutton <mbhutton@gmail.com>
2024-10-17 21:55:34 +1300

    Revert install command line tools step, as they're a prerequisite
    Command line tools are a prerequisite for Chezmoi install
    which is a requisite for mh-install.
    So mh-install can't be the tool to install them.

--- home/dot_config/mh/shared-bin/mh-install.d/impl-macos.sh
+++
@@ -1,9 +0,0 @@
-# shellcheck shell=bash
-#
-# include xcode and command line tools
-#
-
-if [[ ! /usr/bin/git --version ]]; then
-  echo "Installing Xcode Command Line Tools"
-  xcode-select --install || fail "Failed to install Xcode Command Line Tools"
-fi

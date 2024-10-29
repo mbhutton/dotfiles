534bbf25e7fb7ccb1ada3205147cfd4060ececb8
Matt Hutton <mbhutton@gmail.com>
2024-10-29 18:40:18 +1300

    install: use desired components

--- home/dot_config/mh/shared-bin/mh-install.d/impl-apt.sh
+++ home/dot_config/mh/shared-bin/mh-install.d/impl-apt.sh
@@ -21,35 +21,32 @@ function noun_for_apt {
 function asserts_for_apt {
   [[ -f "$APT_PACKAGES_PATH" ]] || fail "Desired APT packages file not found: $APT_PACKAGES_PATH"
   [[ -f "$APT_BUILD_DEPS_PATH" ]] || fail "Desired APT packages to build-deps for, not found: $APT_BUILD_DEPS_PATH"
+  [[ "$(uname -o)" == "GNU/Linux" ]] || fail "APT is Linux-only"
+  command -v apt-get >/dev/null || fail "apt-get command not found"
   echo "asserts_for_apt"
 }
 
 function install_or_update_apt {
-  if [[ "$(uname -o)" == "GNU/Linux" ]] && command -v apt-get >/dev/null; then
-    echo "Updating APT package cache... (sudo)"
-    sudo apt-get update || fail "Failed to update APT cache"
+  echo "Updating APT package cache... (sudo)"
+  sudo apt-get update || fail "Failed to update APT cache"
 
-    local package
-    for package in "${APT_PACKAGES_ARRAY[@]}"; do
-      echo "Installing (if necessary) APT package: $package (sudo)"
-      sudo apt-get install -y "$package" || fail "Failed to install or update APT package: $package"
-    done
+  local package
+  for package in "${APT_PACKAGES_ARRAY[@]}"; do
+    echo "Installing (if necessary) APT package: $package (sudo)"
+    sudo apt-get install -y "$package" || fail "Failed to install or update APT package: $package"
+  done
 
-    local target
-    for target in "${APT_BUILD_DEPS_ARRAY[@]}"; do
-      echo "Installing (if necessary) APT build-dep package: $target (sudo)"
-      sudo apt-get build-dep -y "$target" || fail "Failed to install or update APT build-dep package: $target"
-    done
+  local target
+  for target in "${APT_BUILD_DEPS_ARRAY[@]}"; do
+    echo "Installing (if necessary) APT build-dep package: $target (sudo)"
+    sudo apt-get build-dep -y "$target" || fail "Failed to install or update APT build-dep package: $target"
+  done
 
-    echo "Upgrading APT packages... (sudo)"
-    sudo apt-get upgrade -y || fail "Failed to upgrade packages"
-  fi
+  echo "Upgrading APT packages... (sudo)"
+  sudo apt-get upgrade -y || fail "Failed to upgrade packages"
 }
 
 function doctor_apt {
-  if [[ "$(uname -o)" == "GNU/Linux" ]] && ! command -v apt-get >/dev/null; then
-    echo "apt-get command not found. Update this script to support this non-APT distro"
-    echo "Information about this Linux release:"
-    [[ -f /etc/os-release ]] && cat /etc/os-release  # lsb_release is not always installed
-  fi
+  echo "Information about this Linux release:"
+  [[ -f /etc/os-release ]] && cat /etc/os-release  # lsb_release is not always installed
 }

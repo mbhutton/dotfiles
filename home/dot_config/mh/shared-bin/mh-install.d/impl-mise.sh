fc9ff5c87d52f025a3aceb7ad4068334e0875f16
Matt Hutton <mbhutton@gmail.com>
2024-10-30 14:36:16 +1300

    impl-mise updates

--- home/dot_config/mh/shared-bin/mh-install.d/impl-mise.sh
+++ home/dot_config/mh/shared-bin/mh-install.d/impl-mise.sh
@@ -7,28 +7,24 @@ function noun_for_mise {
 }
 
 function asserts_for_mise {
-  echo "asserts_for_mise"
+  command -v mise >/dev/null || fail "mise is not installed"
 }
 
 function install_or_update_mise {
   # TODO: consider doing this via chezmoi instead. note: mise paranoid mode is more for limiting plugins rather than distrusting config
-  command -v mise >/dev/null && mise trust "$HOME/.config/mise/config.toml"
+  mise trust "$HOME/.config/mise/config.toml"
+  mise upgrade
 
-  command -v mise >/dev/null && mise upgrade
-
+  # TODO why are bootstrap calls somehow not activating mise? maybe *always* activate it?
   if [[ "$PATH" != *"/mise/installs/"* ]]; then
-    command -v mise > /dev/null && eval "$(/usr/bin/env mise activate bash)"
+    eval "$(/usr/bin/env mise activate bash)"
   fi
 }
 
 function doctor_mise {
-  if ! command -v mise >/dev/null; then
-    echo "Mise is not available"
-    return 0
-  fi
   prune_output="$(mise prune --dry-run)"
   if [[ -n "$prune_output" ]]; then
-    echo "Mise reports pruneable packages:"
+    echo "Mise reports packages able to be pruned:"
     echo "$prune_output"
   fi
 

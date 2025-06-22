a1e2081c88175e221b6d2daaf9b956601cfa10a6
Matt Hutton <mbhutton@gmail.com>
2025-06-22 17:45:18 +1200

    Keep npm
    k

--- home/dot_config/mh/shared-bin/mh-install.d/impl-npm.sh
+++ home/dot_config/mh/shared-bin/mh-install.d/impl-npm.sh
@@ -1,5 +1,8 @@
 # shellcheck shell=bash
 
+# TODO: Manager this more explicitly through mise
+# TODO: Consolidate mermaid-cli and claude to use the same approach: probably `npm install -g` using latest node via mise
+
 DESIRED_NPM_PACKAGES_PATH="${HOME}/.config/mh/desired-state/npm"
 
 assert_function mh-parse-desired
@@ -22,18 +25,25 @@ function asserts_for_npm {
 function install_or_update_npm {
   local package
   echo "Installing or updating global npm packages..." ; echo
-  
+
   for package in "${DESIRED_NPM_PACKAGES[@]}"; do
-    echo -n "Installing or updating $package... "
+    echo "Installing or updating $package... "
     npm install -g "$package" \
     || fail "Failed to install or update npm package: $package"
   done
 
-  echo "Updating all global npm packages..."
-  npm update -g
+  # TODO: The install command above should handle updates, making the below redundant.
+  # echo "Updating all global npm packages..."
+  # npm update -g
 }
 
 function doctor_npm {
-  echo "npm_doctor"
+  true
+  # npm doctor TODO: Too verbose for now, would need filtering
   # TODO: check for unexpected global packages using 'npm list -g --depth=0'
-}
+  #   npm list -g --depth=0 --parseable=true
+  # /Users/matt/.local/share/mise/installs/node/24.2.0/lib
+  # /Users/matt/.local/share/mise/installs/node/24.2.0/lib/node_modules/@anthropic-ai/claude-code
+  # /Users/matt/.local/share/mise/installs/node/24.2.0/lib/node_modules/corepack
+  # /Users/matt/.local/share/mise/installs/node/24.2.0/lib/node_modules/npm
+  }
